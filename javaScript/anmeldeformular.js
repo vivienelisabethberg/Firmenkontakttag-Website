d4b1f6eb297a8ee7fd7872de1e49aa5397a20b22
ThiloWittmer <thilo.wittmer@gmail.com>
2022-07-05 15:34:45 +0200

    Email validation fertig

--- javaScript/anmeldeformular.js
+++ javaScript/anmeldeformular.js
@@ -1,13 +1,13 @@
 //Evaluierung Anmeldeformular
 const form = document.getElementById("formId");
-const email = document.getElementById("email").value;
-let errorMessages = [];
+const email = document.getElementById("email");
 
-const errorDiv = document.getElementById("error"); 
+const errorDiv = document.getElementById("error");
 
 form.addEventListener("submit", (e) =>{
-  checkInputs(); 
-  console.log(errorMessages); 
+  let errorMessages = [];
+  checkInputs(errorMessages); 
+  console.log(errorMessages);
   if(errorMessages.length > 0){
     e.preventDefault();//lädt Seite nicht neu 
     errorDiv.innerHTML = errorMessages.join(", "); 
@@ -15,11 +15,22 @@ form.addEventListener("submit", (e) =>{
 }); 
 
 
-function checkInputs(){
-  if(email === ''){
+function checkInputs(errorMessages){
+  if(email.value === ''){
+    console.log(email.value)
     errorMessages.push('Bitte eine Emailadresse eingeben.'); 
-  }
-  if(email.match('/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/')){
+  }else if(!isEmail(email.value)){
+    console.log("email error")
     errorMessages.push('Bitte eine gültige Emailadresse eingeben.'); 
+  } else {
+    console.log("no email error")
   }
 }
+
+const isEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
